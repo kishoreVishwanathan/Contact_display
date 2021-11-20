378a2c16fe7f3eb8289b0bf9dc889804a8a289e2
kishoreVishwanathan <coder@fdeacadbaacafbadcddfcbdededeeaaabd-0.fdeacadbaacafbadcddfcbdededeeaaabd.che.svc.cluster.local>
2021-11-20 16:13:58 +0000

    contact display done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {}
+  
   title = 'angularapp';
+  name: String ='';
+  email: String ='';
+  phoneNumber: String='';
+  image: String='../assets/images.jpeg';
+  contacts :Array<any> = [];
+
+  addContacts(){
+    if(this.name && this.email && (this.phoneNumber&& this.phoneNumber.length>9)) {
+      console.log("FIrst");
+      this.contacts.push({
+        name: this.name,
+        email:this.email,
+        phoneNumber: this.phoneNumber,
+        image: this.image
+      })
+    }
+    console.log(this.contacts);
+  }
+  onFileSelected(event) {
+    if(event.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event1: any) => {
+        console.log(event1,"event1");
+        this.image =event1.target.result;
+      }
+    }
+
+  }
 }
+
